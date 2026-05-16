98e38237cc0a250db0e8763ec44989462bcec9cf
laisccanelaveiga <lais.veiga@aluno.senac.rio>
2026-05-15 21:38:57 -0300

    Atividades com DEF

--- 8. AULA/atividade2.py
+++ 8. AULA/atividade2.py
@@ -9,18 +9,34 @@ peso = float(input('Peso: '))
 indice = calc_imc(altura,peso)
 print(f"\nSeu imc é {indice:.2f}")
 
-if indice > 40:
-    print("Obesidade grau III")
-elif indice >= 35:
-    print("Obesidade grau II")
-elif indice >= 30:
-    print("Obesidade grau I")
-elif indice >= 25:
-    print("Acima do peso")
-elif indice >= 18.5:
-    print("Peso normal")
-elif indice >= 17:
-    print("Abaixo do peso")
-else:
-    print('Muito abaixo do peso')
+# if indice > 40:
+#     print("Obesidade grau III")
+# elif indice >= 35:
+#     print("Obesidade grau II")
+# elif indice >= 30:
+#     print("Obesidade grau I")
+# elif indice >= 25:
+#     print("Acima do peso")
+# elif indice >= 18.5:
+#     print("Peso normal")
+# elif indice >= 17:
+#     print("Abaixo do peso")
+# else:
+#     print('Muito abaixo do peso')
     
+match indice:
+
+    case indice if indice > 40:
+        print("Obesidade grau III")
+    case indice if indice > 35:
+        print("Obesidade grau II")
+    case indice if indice > 30:
+        print("Obesidade grau I")
+    case indice if indice > 25:
+        print("Acima do peso")
+    case indice if indice > 18.5:
+        print("Peso normal")
+    case indice if indice > 17:
+        print("Abaixo do peso")
+    case _:
+        print('Muito abaixo do peso')
